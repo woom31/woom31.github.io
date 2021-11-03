a901699d80e3bc3dbb7ea1c2fba233d83626a5ec
woom31 <72497969+woom31@users.noreply.github.com>
2021-11-03 13:00:08 +0900

    Delete 1212 directory

--- 1212/js/mobileScroll.js
+++
@@ -1,9 +0,0 @@
-var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
-
-jQuery(document).ready(function($){
-    if(!isMobile) {
-        console.log('pc');
-    } else {
-        console.log('mobile');
-    }
-});
